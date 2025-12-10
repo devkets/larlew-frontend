daf4f39ec32bc3dfa07b78f6837c244988eec668
Andrew Lewis <ajlewis1851@gmail.com>
2025-12-10 09:04:14 -0600

    Updating project to allow external calls to localhost:8080 and render the MathComponent

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MathContainer from './components/MathContainer';
 
 function App() {
   return (
     <div>
       <h1>Hello, World!</h1>
       <p>Welcome to my React application.</p>
+      <div>
+        <MathContainer />
+      </div>
+      
     </div>
   );
 }
