f57688dc646eaf0e8b5efdace1284776b48f3be9
Chris Larsen <clarsen12@gmail.com>
2026-01-29 12:59:26 -0700

    Added home.js and app.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MathContainer from './components/math/MathContainer';
-import DiceGameContainer from './components/dice/DiceGameContainer';
-
-function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-      <p>Welcome to my React application.</p>
-      <div>
-        <MathContainer />
-      </div>
-      <div>
-        <DiceGameContainer />
-      </div>
-    </div>
-  );
-}
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
